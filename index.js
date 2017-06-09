0db81cdc5898efd84cfbe30fa7ce8b462ca19fe9
Bach Hung Cong <congbach.bhtech@gmail.com>
2017-06-09 12:51:15 +0700

    modified restify cors

--- index.js
+++ index.js
@@ -17,13 +17,33 @@ server.use(restify.fullResponse())
 //     this.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 // }
 
-server.use(
-    function crossOrigin(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        return next();
+function unknownMethodHandler(req, res) {
+    if (req.method.toLowerCase() === 'options') {
+        console.log('received an options method request');
+        var allowHeaders = ['Accept', 'Accept-Version', 'Content-Type', 'Api-Version', 'Origin', 'X-Requested-With']; // added Origin & X-Requested-With
+
+        if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS');
+
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
+        res.header('Access-Control-Allow-Methods', res.methods.join(', '));
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+
+        return res.send(204);
     }
-);
+    else
+        return res.send(new restify.MethodNotAllowedError());
+}
+
+server.on('MethodNotAllowed', unknownMethodHandler);
+
+// server.use(
+//     function crossOrigin(req, res, next) {
+//         res.header("Access-Control-Allow-Origin", "*");
+//         res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//         return next();
+//     }
+// );
 
 // server.use(function (req, res, next) {
 //     if (req.headers['x-forwarded-proto'] === 'http') {
